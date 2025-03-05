2d22d87aa3d59470003ab08835e26c0fe6335c4a
ab <ab@ab.ab>
2025-03-05 13:21:04 +0200

    try GrpcIoServer as well

--- grpc-server-vertx/src/main/java/hex1r0/VertxApp.java
+++ grpc-server-vertx/src/main/java/hex1r0/VertxApp.java
@@ -1,25 +1,41 @@
 package hex1r0;
 
+import io.grpc.stub.StreamObserver;
 import io.vertx.core.Vertx;
-import io.vertx.grpc.server.GrpcServer;
+import io.vertx.grpcio.server.GrpcIoServer;
+import io.vertx.grpcio.server.GrpcIoServiceBridge;
 
 public class VertxApp {
   public static void main(String[] args) {
     var vertx = Vertx.vertx();
-    var grpcServer = GrpcServer.server(vertx);
+    //    var grpcServer = GrpcServer.server(vertx);
 
-    grpcServer.callHandler(
-        VertxHelloGrpcGrpcServer.SayHello,
-        request ->
-            request.handler(
-                hello -> {
-                  var response = request.response();
+    //    grpcServer.callHandler(
+    //        VertxHelloGrpcGrpcServer.SayHello,
+    //        request ->
+    //            request.handler(
+    //                hello -> {
+    //                  var response = request.response();
+    //
+    //                  HelloReply reply =
+    //                      HelloReply.newBuilder().setMessage("Hello " + hello.getName()).build();
+    //
+    //                  response.end(reply);
+    //                }));
 
-                  HelloReply reply =
-                      HelloReply.newBuilder().setMessage("Hello " + hello.getName()).build();
+    var service =
+        new HelloGrpcGrpc.HelloGrpcImplBase() {
+          @Override
+          public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+            responseObserver.onNext(
+                HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
+            responseObserver.onCompleted();
+          }
+        };
 
-                  response.end(reply);
-                }));
+    var grpcServer = GrpcIoServer.server(vertx);
+    var serverStub = GrpcIoServiceBridge.bridge(service);
+    serverStub.bind(grpcServer);
 
     vertx
         .createHttpServer()
