2921919e9d553abe6c0a575e680e5ceb4698f254
Alex B <oleksandr@exberry.io>
2025-03-29 19:58:13 +0200

    multiple event loops for vertx grpc server

--- grpc-server-vertx/src/main/java/hex1r0/VertxApp.java
+++ grpc-server-vertx/src/main/java/hex1r0/VertxApp.java
@@ -1,50 +1,52 @@
 package hex1r0;
 
 import io.grpc.stub.StreamObserver;
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.grpcio.server.GrpcIoServer;
 import io.vertx.grpcio.server.GrpcIoServiceBridge;
 
 public class VertxApp {
+
+  public static class Server extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+      var service =
+          new HelloGrpcGrpc.HelloGrpcImplBase() {
+            @Override
+            public void sayHello(
+                HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+              responseObserver.onNext(
+                  HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
+              responseObserver.onCompleted();
+            }
+          };
+
+      var grpcServer = GrpcIoServer.server(getVertx());
+      var serverStub = GrpcIoServiceBridge.bridge(service);
+      serverStub.bind(grpcServer);
+
+      getVertx()
+          .createHttpServer()
+          .requestHandler(grpcServer)
+          .listen(8080)
+          .onSuccess(
+              server -> {
+                System.out.println("Server started, listening on " + server.actualPort());
+              })
+          .onFailure(Throwable::printStackTrace);
+    }
+  }
+
   public static void main(String[] args) {
-    var vertx = Vertx.vertx();
-    //    var grpcServer = GrpcServer.server(vertx);
+    var vertx =
+        Vertx.vertx(
+            new VertxOptions().setEventLoopPoolSize(Runtime.getRuntime().availableProcessors()));
 
-    //    grpcServer.callHandler(
-    //        VertxHelloGrpcGrpcServer.SayHello,
-    //        request ->
-    //            request.handler(
-    //                hello -> {
-    //                  var response = request.response();
-    //
-    //                  HelloReply reply =
-    //                      HelloReply.newBuilder().setMessage("Hello " + hello.getName()).build();
-    //
-    //                  response.end(reply);
-    //                }));
-
-    var service =
-        new HelloGrpcGrpc.HelloGrpcImplBase() {
-          @Override
-          public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-            responseObserver.onNext(
-                HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
-            responseObserver.onCompleted();
-          }
-        };
-
-    var grpcServer = GrpcIoServer.server(vertx);
-    var serverStub = GrpcIoServiceBridge.bridge(service);
-    serverStub.bind(grpcServer);
-
-    vertx
-        .createHttpServer()
-        .requestHandler(grpcServer)
-        .listen(8080)
-        .onSuccess(
-            server -> {
-              System.out.println("Server started, listening on " + server.actualPort());
-            })
-        .onFailure(Throwable::printStackTrace);
+    for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+      vertx.deployVerticle(new Server());
+    }
   }
 }
